e21a02db74f191d005db818cfcdf47ba146a1c4a
AswinGans01 <76236532+AswinGans01@users.noreply.github.com>
2020-12-20 00:17:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 * jangan ubah semuanya kecuali nama bot instagram yt itu ajah yg lain jangan!!!
 * hargai pembuat skrip woy!!!
 */
-const XPTN = 'BANGLENT'; // Nama Bot Whatsapp
+const XPTN = 'Aswin bot'; // Nama Bot Whatsapp
 const instagram = 'https://instagram.com/maslent11'; // Nama Instagramlu cok
-const nomer = 'https://Wa.me/+628165466368'; // Nomor whatsapplu cok
+const nomer = 'https://Wa.me/+6281440092890'; // Nomor whatsapplu cok
 const aktif = '2 jam'; // Kapan bot lu aktif
 const groupwa = 'https://chat.whatsapp.com/IKmRs4a36fIAyn7X16jpwL'; // OFFICIAL GRUP LU 1
 const youtube = 'https://www.youtube.com/channel/UCAn_1yox-jIUFhUVYTxI_JA'; // OFFICIAL GRUP LU 2
